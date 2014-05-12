ab11ea8feb32b5550dd857115afa760c7f49ccbf
Petr Korolev <sky4winder@gmail.com>
2014-05-12 15:24:22 +0300

    add ability to plot non-curved graphs

--- TWRCharts/TWRLineChart.h
+++ TWRCharts/TWRLineChart.h
@@ -14,6 +14,7 @@
 @property (copy, nonatomic) NSMutableArray *labels;
 @property (copy, nonatomic) NSMutableArray *dataSets;
 @property (assign, nonatomic) BOOL animated;
+@property(nonatomic) BOOL curveLines;
 
 /**
  *  Initializing the Line Chart object
@@ -27,5 +28,20 @@
 - (instancetype)initWithLabels:(NSArray *)labels
                       dataSets:(NSArray *)dataSets
                       animated:(BOOL)animated;
+
+/**
+ *  Initializing the Line Chart object
+ *
+ *  @param labels   an NSArray of labels (NSString) to go along with the data
+ *  @param dataSets an NSArray of TWRDataSet objects containing the data to be plotted
+ *  @param animated a BOOL defining whether the chart should be animated or not
+ *  @param curved a BOOL defining whether the chart should be curved or not
+ *
+ *  @return an instance of TWRLineChart
+ */
+- (instancetype)initWithLabels:(NSArray *)labels
+                      dataSets:(NSArray *)dataSets
+                      animated:(BOOL)animated
+                        curved:(BOOL)curved;
 
 @end
